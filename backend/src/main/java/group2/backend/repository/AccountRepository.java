ee066e03333567644d003b34e9800df2a67d2d94
Daniels <52871248+Ciket@users.noreply.github.com>
2025-11-06 21:00:11 +0800

    Add findByEmail function to AccountRepository

--- backend/src/main/java/group2/backend/repository/AccountRepository.java
+++ backend/src/main/java/group2/backend/repository/AccountRepository.java
@@ -5,6 +5,9 @@ import group2.backend.model.Account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository <Account, Long> {
+    Optional<Account> findByEmail(String email);
 }
