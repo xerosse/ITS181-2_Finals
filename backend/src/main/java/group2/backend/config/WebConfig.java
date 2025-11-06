a63c659cac1d1d05ca01714834082f26a6f5cdfb
Daniels <52871248+Ciket@users.noreply.github.com>
2025-11-06 21:05:47 +0800

    Add WebConfig to Register AuthInterceptor
    Registers AuthInterceptor to the Spring Server

---
+++ backend/src/main/java/group2/backend/config/WebConfig.java
@@ -0,0 +1,15 @@
+package group2.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/api/**"); // apply to API endpoints
+    }
+}
