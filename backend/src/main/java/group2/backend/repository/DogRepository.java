9a75cb031b6f6ae4f43aaea13289cc974182d832
Daniels <52871248+Ciket@users.noreply.github.com>
2025-11-10 09:23:54 +0800

    Extend CrudRepo -> JpaRepo in DogRepository.java
    More advanced operations for the planned Search and Filter feature.

--- backend/src/main/java/group2/backend/repository/DogRepository.java
+++ backend/src/main/java/group2/backend/repository/DogRepository.java
@@ -2,9 +2,10 @@ package group2.backend.repository;
 
 
 import group2.backend.model.Dog;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DogRepository extends CrudRepository <Dog, Long> {
+public interface DogRepository extends JpaRepository<Dog, Long>, JpaSpecificationExecutor<Dog> {
 }
