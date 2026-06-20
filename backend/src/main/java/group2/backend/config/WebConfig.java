0cbb4dca718b138fc0dce3bd99745ebd8c8550ab
xerosse <69449089+xerosse@users.noreply.github.com>
2026-06-20 17:34:05 +0800

    cors issue

--- backend/src/main/java/group2/backend/config/WebConfig.java
+++ backend/src/main/java/group2/backend/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                // .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://finals-dog-adoption-site.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie")
